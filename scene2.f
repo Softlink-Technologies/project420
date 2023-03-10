c8613698e18857663861ac67aa8f264da2900911
SACHIN <119405675+sachin-fullstack@users.noreply.github.com>
2023-03-10 17:20:34 +0530

    Create scene2.f

---
+++ scene2.f
@@ -0,0 +1,13 @@
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+      ),
+      body: Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
